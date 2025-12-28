4d6aa287079148f80b207eea63148bf3b10eb523
Senkex <ssenkex@gmail.com>
2025-12-28 15:31:12 -0300

    feat(project): Initialize new ApoloLangs project structure

---
+++ src/main/java/com/github/nautic/ApoloLangs.java
@@ -0,0 +1,15 @@
+package com.github.nautic;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ApoloLangs extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
